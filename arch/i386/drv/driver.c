ed2e00f98100f8395cd208edfdcbc986f2fd95b7
amanuel2 <amanuelb43@gmail.com>
2016-12-15 20:52:03 +0000

    Changes setup

--- arch/i386/drv/driver.c
+++ arch/i386/drv/driver.c
@@ -76,13 +76,13 @@ void setup_driver_handler()
   pit_driver.status = status_pit;
   kbd_driver.initalized = initalized_ps2_kbd;
   kbd_driver.status = status_ps2_kbd;
-  
-  printk("%d" , pit_driver.status);
+
   
   /*Initalize and Uninitalize functions for Video Driver*/
   video_driver.init = video_drivers[VGA_VIDEO_DRIVER_INDEX]->init;
   video_driver.uninit = video_drivers[VGA_VIDEO_DRIVER_INDEX]->uninit;
   video_driver.status = video_drivers[VGA_VIDEO_DRIVER_INDEX]->status;
+  
 }
 
 //check if device is initalized
--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #Install Dependencies
 
 echo "SETTING UP DEPENDENCIES"
@@ -7,24 +6,25 @@ if [ "$(uname)" == "Darwin" ]; then
    sleep 2
    brew install nasm
    brew install gcc
-   brew install gcc-multilib
    brew install qemu
    brew install grub-common
    brew install xorriso
-   brew install grub-pc-bin
-   brew install bochs
+   brew install grub-pc-bin   
+   brew install easy_install 
+   easy_install supervisor
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+   echo pwd
    echo "RUNNING UNDER a linux distro"
     sleep 2
     sudo apt-get update
     sudo apt-get install nasm
     sudo apt-get install gcc
-    sudo apt-get install gcc-multilib
     sudo apt-get install qemu
     sudo apt-get install grub-common
     sudo apt-get install xorriso
     sudo apt-get install grub-pc-bin
-    sudo apt-get install bochs
+    sudo apt-get install easy_install 
+    easy_install supervisor
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under Windows NT platform
     echo "BoneOS Isnt Available on Windows Operating Systems Yet."
