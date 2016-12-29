0bbe01f355457be3648749ca17cd361135de18be
Rishav Kundu <rishav.kundu98@gmail.com>
2016-12-30 04:39:06 +0530

    Remove apt upgrade from setup.sh
    I don't want to upgrade my Firefox...

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 echo "SETTING UP DEPENDENCIES"
 if [ "$(uname)" == "Darwin" ]; then
   echo "RUNNING UNDER Max OSX"
-   sleep 2
    brew install nasm
    brew install gcc
    brew install gcc-multilib
@@ -15,17 +14,8 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
    echo pwd
    echo "RUNNING UNDER a linux distro"
-    sleep 2
     sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install nasm
-    sudo apt-get install gcc
-     sudo apt-get install gcc-multilib
-    sudo apt-get install qemu
-    sudo apt-get install grub-common
-    sudo apt-get install xorriso
-    sudo apt-get install grub-pc-bin
-    sudo apt-get install supervisor
+    sudo apt-get install nasm gcc gcc-multilib qemu grub-common xorriso grub-pc-bin supervisor
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under Windows NT platform
     echo "BoneOS Isnt Available on Windows Operating Systems Yet."
