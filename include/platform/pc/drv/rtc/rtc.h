bf5395f2fce9fc9100f52f9a7dac739e7f1db183
Amanuel <amanuelb43@gmail.com>
2017-01-01 14:17:29 -0500

    Update rtc.h

--- include/platform/pc/drv/rtc/rtc.h
+++ include/platform/pc/drv/rtc/rtc.h
@@ -11,6 +11,17 @@ typedef struct uint8_rtc
 	uint8_t second;
 } rtc_t;
 extern rtc_t rtc_get();
+
+
 extern void rtc_print_time ();
+
+
+extern uint8_t rtc_get_year();
+extern uint8_t rtc_get_month();
+extern uint8_t rtc_get_day();
+extern uint8_t rtc_get_hour();
+extern uint8_t rtc_get_minute();
+extern uint8_t rtc_get_second();
+
 
 #endif // _DRV_RTC_H_
