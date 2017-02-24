d8b23f33757820042a998447dde07cdcadda68c3
amanuel2 <amanuelb43@gmail.com>
2017-02-24 18:15:08 +0000

    Changes PCI

--- include/platform/pc/drv/pci/pci.h
+++ include/platform/pc/drv/pci/pci.h
@@ -2,14 +2,15 @@
 #define _DRV_PCI_H_ 
 
 //32 bit I/O Locations
-#define CONFIG_ADDRESS 0xCF8
-#define CONFIG_DATA    0xCFC
+#define PORT_CONFIG_ADDRESS 0xCF8
+#define PORT_CONFIG_DATA    0xCFC
 
 typedef struct
 {
-    
+    uint32_t vendor_id;
+    uint32_t class_id;
 }pci_device_t;
 
-extern uint32_t get_address(void);
+extern uint32_t pci_get_address(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 
 #endif /*_DRV_PCI_H_*/
--- platform/pc/drv/pci/pci.c
+++ platform/pc/drv/pci/pci.c
@@ -21,4 +21,14 @@
  **     Amanuel Bogale <amanuel2> : start
  **/
  
+ #include <stdargs.h>
+ #include <stdint.h>
  
+ uint32_t pci_get_address(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
+ {
+     return ( (uint32_t) 
+     
+              ((bus << 16) | (slot << 11) |
+              (func << 8) | (offset & 0xfc) | ((uint32_t)0x80000000)); 
+            )
+ }
